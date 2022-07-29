b6b22089789eb4b2bdf5c5ee7836d5c0ddb3b338
Guillermo De Los Santos <franmen05@gmail.com>
2022-07-29 15:02:30 -0400

    first  commit

---
+++ src/main/java/com/guille/service/AccountService.java
@@ -0,0 +1,14 @@
+package com.guille.service;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Set;
+
+@ApplicationScoped
+public class AccountService {
+
+    public Object read(Object file){
+
+        return new Object();
+
+    }
+}
