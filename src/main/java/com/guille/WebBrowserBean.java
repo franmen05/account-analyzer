c407ad26acd7f0e320e86b3e7f1d870f2e61b9c1
franmen05 <franmen05@gmail.com>
2023-08-06 19:19:55 -0400

    improve style of deductions

--- src/main/java/com/guille/WebBrowserBean.java
+++ src/main/java/com/guille/WebBrowserBean.java
@@ -3,6 +3,10 @@ package com.guille;
 import io.quarkus.runtime.Startup;
 
 import javax.inject.Singleton;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 // import java.awt.*;
 // import java.io.IOException;
 // import java.net.URI;
@@ -11,21 +15,24 @@ import javax.inject.Singleton;
 @Startup
 @Singleton
 public class WebBrowserBean {
+//    @ConfigProperty(name = "quarkus.http.port", defaultValue = "8082")
+    String assignedPort="8082";
 
     WebBrowserBean() {
-        //open();
+        open();
     }
 
-//    public void open()  {
-//        var url = "http://localhost:8080/index.html";
-//        try {
-//            if(Desktop.isDesktopSupported())
-//                Desktop.getDesktop().browse(new URI(url));
+    public void open()  {
+        var url = "http://localhost:"+assignedPort+"/index.html";
+
+        try {
+            if(Desktop.isDesktopSupported())
+                Desktop.getDesktop().browse(new URI(url));
 //            else
 //                Runtime.getRuntime().exec("xdg-open " + url);
-//
-//        } catch (IOException | URISyntaxException e) {
-//            e.printStackTrace();
-//        }
-//    }
+
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 }
