cfd6b7b842a852c127f1bd3596ca278bfe0d59ed
franmen05 <franmen05@gmail.com>
2023-08-19 20:48:50 -0400

    Add zip for distribution

--- src/main/java/com/guille/domain/Transaction.java
+++ src/main/java/com/guille/domain/Transaction.java
@@ -1,25 +1,18 @@
 package com.guille.domain;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
-public record Transaction(String date, String type, Float amount , Integer referenceNumber, String serial, String desc) {
+public record Transaction(String date, String type, Float amount, Integer referenceNumber, String serial, String desc) {
 
 
-//    public Boolean compareDesc(String _desc) {
-//        return desc.equalsIgnoreCase(_desc);
-//    }
-//    public Boolean isSerial(String _serial) {
-//        return serial.equalsIgnoreCase(_serial);
-//    }
     public Boolean descContains(String _desc) {
         return desc.toLowerCase().contains(_desc.toLowerCase());
     }
+
     public Boolean descContains(Set<String> _desc) {
         return _desc.stream().map(String::toLowerCase)
                 .anyMatch(s -> desc.toLowerCase().contains(s));
     }
-
 
     @Override
     public String toString() {
